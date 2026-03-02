fa58273e4c1c1a81da741df1f5534ea13e35f891
Łukasz Kułacz <lukasz.kulacz@rimedolabs.com>
2026-03-02 17:13:21 +0100

    Punkt startowy lekcji pierwszej
    Dodano pliki startowe dostępne w lekcji pierwszej

---
+++ main.py
@@ -0,0 +1,68 @@
+from pydantic import BaseModel
+from typing import Dict
+
+import json
+
+
+class Parameters(BaseModel):
+    apartments_json_path: str = 'data/apartments.json'
+    tenants_json_path: str = 'data/tenants.json'
+
+
+class Room(BaseModel):
+    name: str
+    area_m2: float
+
+
+class Apartment(BaseModel):
+    key: str
+    name: str
+    location: str
+    area_m2: bool
+    rooms: Dict[str, Room]
+
+    @staticmethod
+    def from_json_file(file_path: str) -> Dict[str,'Apartment']:
+        data = None
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        assert isinstance(data, dict), "Expected a dictionary of apartments"
+        return {key: Apartment(**apartment) for key, apartment in data.items()}
+
+    
+class Tenant(BaseModel):
+    name: str
+    apartment: str
+    room: str
+    rent_pln: float
+    deposit_pln: float
+    date_agreement_from: str
+    date_agreement_to: str
+
+    @staticmethod
+    def from_json_file(file_path: str) -> Dict[str,'Tenant']:
+        data = None
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        assert isinstance(data, dict), "Expected a dictionary of tenants"
+        return {key: Tenant(**tenant) for key, tenant in data.items()}
+    
+
+class Manager:
+    def __init__(self, parameters: Parameters):
+        self.parameters = parameters 
+
+        self.apartments = {}
+        self.tenants = {}
+       
+        self.load_data()
+
+    def load_data(self):
+        self.apartments = Apartment.from_json_file(self.parameters.apartments_json_path)
+
+
+if __name__ == '__main__':
+    parameters = Parameters()
+    manager = Manager(parameters)
+
+    
